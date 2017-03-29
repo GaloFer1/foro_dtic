a9d8feedb61587874498f316b300da82f9916f27
diegofertr <diegonano09@gmail.com>
2017-03-29 18:09:21 -0400

    Tutorials module

--- resources/views/tutorials.blade.php
+++ resources/views/tutorials.blade.php
@@ -73,7 +73,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Tutoriales
+                    Tutoriales de Desarrollo web
                 </div>
             </div>
         </div>
