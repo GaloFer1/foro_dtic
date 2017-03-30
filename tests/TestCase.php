b393dece8ae61e50551827fec28d7e7b51e34a30
diegofertr <diegonano09@gmail.com>
2017-03-30 16:17:21 -0400

    Add Testing create_a_user_post

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -10,6 +10,10 @@ abstract class TestCase extends Illuminate\Foundation\Testing\TestCase
     protected $baseUrl = 'http://localhost';
 
     /**
+    * @var \App\User
+    */
+    protected $defaultUser;
+    /**
      * Creates the application.
      *
      * @return \Illuminate\Foundation\Application
--- tests/features/CreatePostTest.php
+++ tests/features/CreatePostTest.php
@@ -2,10 +2,30 @@
 
 class CreatePostTest extends FeatureTestCase
 {
+    public function test_a_user_create_a_post()
+    {
+        //Having (teniendo una pregunta)
+        $title = 'Esta es una pregunta';
+        $content = 'Este es el contenido';
 
-  function test_a_user_create_a_post(){
+        //Usuario que se conecta
+        $this->actingAs($this->defaultUser());
 
+        //When (eventos de la prueba)
+        $this->visit(route('post.create'))
+             ->type($title, 'title')
+             ->type($content, 'content')
+             ->press('Publicar');
 
-  }
+        //Then (resultado)
+        //para ver si el usuario fue creado correctamente en la base de datos
+        $this->seeInDatabase('posts', [
+             'title' => $title,
+             'content' => $content,
+             'pending' => true,
+        ]);
 
+        //Test user is redirect to the posts details after creating it.
+        $this->seeInElement('h1', $title);
+    }
 }
